6bfe1f83d3664a1bc51b0d9080bdb4c7cf6833b0
MD931 <m.habchi12@gmail.com>
2017-12-11 17:43:57 +0100

    add new API

---
+++ src/main/java/com/surirobot/services/algorithmia/EmotionAlgorithmia.java
@@ -0,0 +1,68 @@
+package com.surirobot.services.algorithmia;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+import com.surirobot.services.interfaces.IService;
+import com.surirobot.utils.EnvVar;
+
+public class EmotionAlgorithmia implements IService<String, byte[]>{
+
+private static final Logger logger = LogManager.getLogger();
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.surirobot.services.interfaces.IService#getEmotions(java.lang.Object)
+	 * Recuperer la réponse de Algorithmia
+	 */
+	@Override
+	public JSONObject getEmotions(String image) {
+		logger.info("EmotionAlgorithmia : start getEmotions");
+		JSONObject json = new Parser().parse(send(decoder(image)));
+		return json;
+	}
+	
+	/*
+	 * Méthode qui nous convertit une image en base64 vers du binaire
+	 */
+	public static byte[] decoder(String base64Image) {
+		return Base64.getDecoder().decode(base64Image);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.surirobot.services.interfaces.IService#send(java.lang.Object)
+	 * Méthode qui s'occupe d'appeler Algorithmia et récuperer le résultat
+	 */
+	@Override
+	public HttpResponse send(byte[] image) {
+		logger.info("EmotionAlgorithmia : start send");
+		
+		HttpClient client = new DefaultHttpClient();  
+		HttpPost request = new HttpPost("https://api.algorithmia.com/v1/algo/deeplearning/EmotionRecognitionCNNMBP/1.0.1");
+		request.setHeader("Content-Type", "application/octet-stream");
+		request.setHeader("Authorization", "Simple "+System.getenv(EnvVar.API_ALGORITHMIA.toString()));
+		request.setEntity(new ByteArrayEntity(image, ContentType.APPLICATION_OCTET_STREAM));
+		HttpResponse response = null;
+		try {
+			response = client.execute(request);
+		} catch (ClientProtocolException e) {
+			logger.error("Problème de protocole...\n"+e.getStackTrace());
+		} catch (IOException e) {
+			logger.error("Problème d'entrée sortie...\n"+e.getStackTrace());
+		}
+		return response;
+	}
+
+}
---
+++ src/main/java/com/surirobot/services/algorithmia/Parser.java
@@ -0,0 +1,76 @@
+package com.surirobot.services.algorithmia;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.surirobot.services.interfaces.IParser;
+import com.surirobot.utils.Emotion;
+
+/*
+ * Cette class permet de parser le résutat de la réponse de l'API intérogée. 
+ */
+public class Parser implements IParser{
+	private static final Logger logger = LogManager.getLogger();
+	//private static final String[] emotions = {"Happy", "Neutral", "Sad", "Angry", "Disgust","Fear", "Surprise"};
+	private static Map<String, String> emotions = null;
+
+	public static void initializeMap() {
+		if(emotions == null) {
+			emotions = new HashMap<>();
+			emotions.put("Sad", Emotion.SADNESS.toString());
+			emotions.put("Happy", Emotion.HAPPINESS.toString());
+			emotions.put("Angry", Emotion.ANGER.toString());
+			emotions.put("Neutral", Emotion.NEUTRAL.toString());
+			emotions.put("Disgust", Emotion.DISGUST.toString());
+			emotions.put("Fear", Emotion.FEAR.toString());
+			emotions.put("Surprise", Emotion.SURPRISE.toString());
+		}
+	}
+
+
+	/*
+	 * Cette méthode permet de parser le résultat <pre>response</pre> retourné 
+	 * par l'API ,sous form d'un <pre>JSONOBject</pre> contenant les émotions 
+	 *  basiques.
+	 */
+	@Override
+	public JSONObject parse(HttpResponse response) {
+		logger.info("EmotionAlgorithmia : start parse");
+		initializeMap();
+		if(response.getStatusLine().getStatusCode() != 200) return new JSONObject();
+		String s = "";
+		try {
+			BufferedReader bis = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			String tmp = null;
+			while((tmp = bis.readLine())!=null) {
+				s += tmp;
+			}
+		} catch (UnsupportedOperationException | IOException e) {
+			e.printStackTrace();
+		}
+		JSONObject json = new JSONObject(s);
+		JSONArray results = json.getJSONObject("result").optJSONArray("results");
+		if(results.length()<1) return new JSONObject();
+		JSONArray tmp = results.getJSONObject(0).optJSONArray("emotions");
+		JSONObject score = new JSONObject();
+		JSONObject js;
+		for(int i =0;i<tmp.length();++i) {
+			js = tmp.getJSONObject(i);
+			String emotion = emotions.get(js.optString("label"));
+			score.put(emotion.toLowerCase(), js.optDouble("confidence"));
+		}
+		System.out.println("TEST"+score.toString());
+		return new JSONObject().put("scores", score);
+	}
+
+}
+
